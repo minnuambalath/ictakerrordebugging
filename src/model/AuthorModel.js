d9125cba7686e25e3592990defdb531223300eb3
minnuambalath <minnuambalath@gmail.com>
2022-05-22 19:38:06 +0400

    vese1

---
+++ src/model/AuthorModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://minnuambalath:minnu9821@cluster0.tqvko.mongodb.net/?retryWrites=true&w=majority');
+const Schema = mongoose.Schema;
+
+
+const AuthorSchema = new Schema({
+    title : String,
+    image: String,
+    about: String
+});
+
+const authordata = mongoose.model('authordata',AuthorSchema);
+
+module.exports = authordata;
